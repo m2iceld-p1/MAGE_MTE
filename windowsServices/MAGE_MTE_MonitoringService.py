02b0fb12643e40549c49f18d05836b8f008661b1
ChrisVerschelden <71574021+ChrisVerschelden@users.noreply.github.com>
2024-02-29 14:58:22 +0100

    Added readme and requirement for pyinstaller

--- windowsServices/MAGE_MTE_MonitoringService.py
+++ windowsServices/MAGE_MTE_MonitoringService.py
@@ -1,45 +1,71 @@
-import servicemanager
-import socket
-import sys
-import win32event
-import win32service
-import win32serviceutil
+# import servicemanager
+# import socket
+# import sys
+# import win32event
+# import win32service
+# import win32serviceutil
 
-class MAGE_MTE_MonitoringService(win32serviceutil.ServiceFramework):
-    _svc_name_ = "MAGE_MTE_MonitoringService" #Service Name (exe)
-    _svc_display_name_ = "MAGE_MTE_MonitoringService" #Service Name which will display in the Winfows Services Window 
-    _svc_description_ = "" ##Service Name which will display in the Winfows Services Window
+# class MAGE_MTE_MonitoringService(win32serviceutil.ServiceFramework):
+#     _svc_name_ = "MAGE_MTE_MonitoringService" #Service Name (exe)
+#     _svc_display_name_ = "MAGE_MTE_MonitoringService" #Service Name which will display in the Winfows Services Window 
+#     _svc_description_ = "" ##Service Name which will display in the Winfows Services Window
 
-    def __init__(self, args):
-        '''
-        Used to initialize the service utility. 
-        '''
-        win32serviceutil.ServiceFramework.__init__(self, args)
-        self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
-        socket.setdefaulttimeout(60)
+#     def __init__(self, args):
+#         '''
+#         Used to initialize the service utility. 
+#         '''
+#         win32serviceutil.ServiceFramework.__init__(self, args)
+#         self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
+#         socket.setdefaulttimeout(60)
 
-    def SvcStop(self):
-        '''
-        Used to stop the service utility (restart / timeout / shutdown)
-        '''
-        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
-        win32event.SetEvent(self.hWaitStop)
+#     def SvcStop(self):
+#         '''
+#         Used to stop the service utility (restart / timeout / shutdown)
+#         '''
+#         self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+#         win32event.SetEvent(self.hWaitStop)
 
-    def SvcDoRun(self):
-        '''
-        Used to execute all the piece of code that you want service to perform.
-        '''
-        rc = None
-        while rc != win32event.WAIT_OBJECT_0:
+#     def SvcDoRun(self):
+#         '''
+#         Used to execute all the piece of code that you want service to perform.
+#         '''
+#         rc = None
+#         while rc != win32event.WAIT_OBJECT_0:
+#             with open('C:\\MAGE_MTE_MonitoringService.log', 'a') as f:
+#                 f.write('MAGE_MTE_MonitoringService running...\n')
+#             #rc = win32event.WaitForSingleObject(self.hWaitStop, 10)
+
+
+# if __name__ == '__main__':
+#     if len(sys.argv) == 1:
+#         servicemanager.Initialize()
+#         servicemanager.PrepareToHostSingle(MAGE_MTE_MonitoringService)
+#         servicemanager.StartServiceCtrlDispatcher()
+#     else:
+#         win32serviceutil.HandleCommandLine(MAGE_MTE_MonitoringService)
+
+import time
+import random
+from pathlib import Path
+from SMWinservice import SMWinservice
+
+class MAGE_MTE_MonitoringService(SMWinservice):
+    _svc_name_ = "MAGE_MTE_MonitoringService"
+    _svc_display_name_ = "MAGE_MTE_MonitoringService"
+    _svc_description_ = "That's a great winservice! :)"
+
+    def start(self):
+        self.isrunning = True
+
+    def stop(self):
+        self.isrunning = False
+
+    def main(self):
+        i = 0
+        while self.isrunning:
             with open('C:\\MAGE_MTE_MonitoringService.log', 'a') as f:
                 f.write('MAGE_MTE_MonitoringService running...\n')
-            #rc = win32event.WaitForSingleObject(self.hWaitStop, 10)
-
+            time.sleep(10)
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        servicemanager.Initialize()
-        servicemanager.PrepareToHostSingle(MAGE_MTE_MonitoringService)
-        servicemanager.StartServiceCtrlDispatcher()
-    else:
-        win32serviceutil.HandleCommandLine(MAGE_MTE_MonitoringService)
+    MAGE_MTE_MonitoringService.parse_command_line()
---
+++ windowsServices/README.md
@@ -0,0 +1,5 @@
+.\MAGE_MTE_MonitoringService\Scripts\activate
+
+pip install -r .\requirements.txt
+
+.\MAGE_MTE_MonitoringService\Scripts\pyinstaller.exe -F .\MAGE_MTE_MonitoringService.py
